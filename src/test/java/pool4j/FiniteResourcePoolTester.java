a1425627e4abee3f6907e6e1db61a19e07c957f7
evanworley <evanworley@470ab876-ca55-0410-ae3e-c9b11ac5761d>
2008-09-01 07:50:36 +0000

    Adding tests

---
+++ src/test/java/pool4j/FiniteResourcePoolTester.java
@@ -0,0 +1,39 @@
+package pool4j;
+
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+
+
+/**
+ * Tests the unique implementation details of <code>FiniteResourcePool</code>. 
+ * @see ResourcePoolTester ResourcePoolTester - for basic API testing
+ * @author Evan Worley
+ */
+public class FiniteResourcePoolTester {
+    
+    @Test
+    public void throwsExceptionWhenRequestOnEmptyPool() {
+        final FiniteResourcePool<Integer> resourcePool = 
+            new FiniteResourcePool<Integer>(Arrays.asList(new Integer[] {1,2,3}));
+        
+        while (!resourcePool.isEmpty()) {
+            resourcePool.getResource();
+        }
+        
+        try {
+            resourcePool.getResource();
+            fail("Expecting a EmptyResourcePoolException to be thrown");
+        } catch (EmptyResourcePoolException e) {
+            // Expected
+        }
+    }
+    
+    @Test(expected = EmptyResourcePoolException.class)
+    public void requiresResources() {
+        new FiniteResourcePool<Integer>(Collections.EMPTY_LIST);
+    }
+}
